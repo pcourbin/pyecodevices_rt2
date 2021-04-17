77096a220f283acd2ef5e112be174e8183e850c3
Pierre COURBIN <pierre.courbin@gmail.com>
2021-04-17 22:37:56 +0200

    Bump version: 1.0.2 → 1.1.0

--- pyecodevices_rt2/__init__.py
+++ pyecodevices_rt2/__init__.py
@@ -16,7 +16,7 @@ from .post import Post
 
 __author__ = """Pierre COURBIN"""
 __email__ = 'pierre.courbin@gmail.com'
-__version__ = '1.0.2'
+__version__ = '1.1.0'
 
 __all__ = [
     "EcoDevicesRT2",
