59cdb5a9cf869834ca9bb1b1ef450cfc2abb82b0
Pierre COURBIN <pierre.courbin@gmail.com>
2022-08-07 23:47:06 +0200

    Bump version: 1.3.0 → 1.3.1

--- pyecodevices_rt2/__init__.py
+++ pyecodevices_rt2/__init__.py
@@ -16,7 +16,7 @@ from .xthl import XTHL
 
 __author__ = """Pierre COURBIN"""
 __email__ = "pierre.courbin@gmail.com"
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 __all__ = [
     "EcoDevicesRT2",
