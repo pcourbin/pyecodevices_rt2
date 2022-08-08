de4e57956c59452017cc20bcd26f7b34144d7545
Pierre COURBIN <pierre.courbin@gmail.com>
2022-08-08 11:22:37 +0200

    Force cached=0 when change value of switch

--- pyecodevices_rt2/abstractswitch.py
+++ pyecodevices_rt2/abstractswitch.py
@@ -48,21 +48,21 @@ class AbstractSwitch:
 
     def on(self) -> bool:
         """Turn on a the AbstractSwitch."""
-        response = self._ecort2.get(self._on_link % (self._id))
+        response = self._ecort2.get(self._on_link % (self._id), cached_ms=0)
         time.sleep(0.01)
         self.get_status(cached_ms=0)
         return response[RESPONSE_ENTRY] == RESPONSE_SUCCESS_VALUE
 
     def off(self) -> bool:
         """Turn off a AbstractSwitch."""
-        response = self._ecort2.get(self._off_link % (self._id))
+        response = self._ecort2.get(self._off_link % (self._id), cached_ms=0)
         time.sleep(0.01)
         self.get_status(cached_ms=0)
         return response[RESPONSE_ENTRY] == RESPONSE_SUCCESS_VALUE
 
     def toggle(self) -> bool:
         """Toggle a AbstractSwitch."""
-        response = self._ecort2.get(self._toggle_link % (self._id))
+        response = self._ecort2.get(self._toggle_link % (self._id), cached_ms=0)
         time.sleep(0.01)
         self.get_status(cached_ms=0)
         return response[RESPONSE_ENTRY] == RESPONSE_SUCCESS_VALUE
